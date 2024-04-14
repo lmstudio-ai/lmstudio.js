c30ea42a2f1b68d7c02c7afa3a1613f8e9c093f4
Ryan Huang <ryan@lmstudio.ai>
2024-04-14 15:03:42 -0400

    Correct the SDK used

--- examples/create-react-app/src/App.tsx
+++ examples/create-react-app/src/App.tsx
@@ -1,11 +1,25 @@
-import { useEffect, useState } from 'react';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
-import { DownloadedModel, LLMLoadModelOpts, LLMPredictionStats, LMStudioClient, OngoingPrediction } from '@lmstudio/sdk-test';
+import {
+  DownloadedModel,
+  LLMLoadModelOpts,
+  LLMPredictionStats,
+  LMStudioClient,
+  OngoingPrediction,
+} from "@lmstudio/sdk";
 
-function LoadedModelWidget({ client, loadedModels, onUnloadModel } : { client: LMStudioClient, loadedModels: {identifier: string, address: string}[], onUnloadModel: (model: string) => void }) {
+function LoadedModelWidget({
+  client,
+  loadedModels,
+  onUnloadModel,
+}: {
+  client: LMStudioClient;
+  loadedModels: { identifier: string; address: string }[];
+  onUnloadModel: (model: string) => void;
+}) {
   const [selectedModel, setSelectedModel] = useState(loadedModels[0].address);
-  const [message, setMessage] = useState('');
+  const [message, setMessage] = useState("");
   const [stats, setStats] = useState<LLMPredictionStats | null>(null);
   const [isPredicting, setIsPredicting] = useState(false);
   const [prediction, setPrediction] = useState<OngoingPrediction | null>(null);
@@ -17,7 +31,7 @@ function LoadedModelWidget({ client, loadedModels, onUnloadModel } : { client: L
     const model = client.llm.get({ identifier: selectedModel });
     const prediction = model?.complete(message);
     setPrediction(prediction);
-    
+
     setIsPredicting(true);
     for await (const fragment of prediction) {
       console.log(fragment);
@@ -30,69 +44,77 @@ function LoadedModelWidget({ client, loadedModels, onUnloadModel } : { client: L
   };
 
   const handleStopPrediction = () => {
-    console.log('Stopping prediction...');
+    console.log("Stopping prediction...");
     prediction?.cancel();
     setIsPredicting(false);
   };
 
   const handleClear = () => {
-    setMessage('');
+    setMessage("");
     setStats(null);
     setPrediction(null);
   };
 
   return (
-    <div style={{ 
-      display: 'flex', flexDirection: 'column', gap: '10px', alignItems: 'center', backgroundColor: 'lightgray', padding: '30px',
-      width: '50%', margin: 'auto', marginTop: '20px'
-    }}>
-        <h1>Prompt an LLM</h1>
-        <div style={{ display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center' }}>
-          <span>🤖</span>
-          <select
-            style={{ height: '22px' }}
-            value={selectedModel}
-            onChange={(e) => setSelectedModel(e.target.value)}
-          >
-            {loadedModels.map((model, index) => (
-              <option key={index} value={model.identifier}>
-                {model.identifier}
-              </option>
-            ))}
-          </select>
-          <button onClick={() => onUnloadModel(selectedModel)}>Unload</button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        gap: "10px",
+        alignItems: "center",
+        backgroundColor: "lightgray",
+        padding: "30px",
+        width: "50%",
+        margin: "auto",
+        marginTop: "20px",
+      }}
+    >
+      <h1>Prompt an LLM</h1>
+      <div style={{ display: "flex", flexDirection: "row", gap: "10px", alignItems: "center" }}>
+        <span>🤖</span>
+        <select
+          style={{ height: "22px" }}
+          value={selectedModel}
+          onChange={e => setSelectedModel(e.target.value)}
+        >
+          {loadedModels.map((model, index) => (
+            <option key={index} value={model.identifier}>
+              {model.identifier}
+            </option>
+          ))}
+        </select>
+        <button onClick={() => onUnloadModel(selectedModel)}>Unload</button>
+      </div>
+      <textarea
+        value={message}
+        placeholder="Type your message here..."
+        onChange={e => setMessage(e.target.value)}
+        style={{ width: "100%", height: "100px", padding: "10px", fontSize: "16px" }}
+      />
+      {!isPredicting ? (
+        <div style={{ display: "flex", flexDirection: "row", gap: "10px", alignItems: "center" }}>
+          <button onClick={handleSubmit}>🔮 Predict</button>
+          <button onClick={handleClear}>🔄 Start Over</button>
         </div>
-        <textarea
-          value={message}
-          placeholder="Type your message here..."
-          onChange={(e) => setMessage(e.target.value)} 
-          style={{ width: '100%', height: '100px', padding: '10px', fontSize: '16px' }}
-        />
-        {
-          !isPredicting ? 
-          <div style={{ display: 'flex', flexDirection: 'row', gap: '10px', alignItems: 'center' }}>
-            <button onClick={handleSubmit}>🔮 Predict</button>
-            <button onClick={handleClear}>🔄 Start Over</button>
+      ) : (
+        <button onClick={handleStopPrediction}>Stop Prediction</button>
+      )}
+
+      <div style={{ width: "100%", padding: "10px", fontSize: "16px", textAlign: "left" }}>
+        {stats && (
+          <div style={{ width: "100%", padding: "10px", fontSize: "12px", textAlign: "left" }}>
+            <pre>Prediction Stats</pre>
+            <pre>{JSON.stringify(stats, null, 2)}</pre>
           </div>
-          : 
-          <button onClick={handleStopPrediction}>Stop Prediction</button>
-        }
-        
-        <div style={{ width: '100%', padding: '10px', fontSize: '16px', textAlign: 'left' }}>
-          {stats &&
-            <div style={{ width: '100%', padding: '10px', fontSize: '12px', textAlign: 'left' }}>
-              <pre>Prediction Stats</pre>
-              <pre>{JSON.stringify(stats, null, 2)}</pre>
-            </div>
-          }
-          </div>
+        )}
+      </div>
     </div>
   );
 }
 
 function App() {
   const [models, setModels] = useState<DownloadedModel[]>([]);
-  const [loadedModels, setLoadedModels] = useState<{identifier: string, address: string}[]>([]);
+  const [loadedModels, setLoadedModels] = useState<{ identifier: string; address: string }[]>([]);
   const [loadingProgress, setLoadingProgress] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [isUnloading, setIsUnloading] = useState(false);
@@ -100,15 +122,15 @@ function App() {
   const client = new LMStudioClient();
 
   useEffect(() => {
-    client.system.listDownloadedModels().then((downloadedModels) => {
+    client.system.listDownloadedModels().then(downloadedModels => {
       setModels(downloadedModels);
     });
   }, []);
 
   useEffect(() => {
-    client.llm.listLoaded().then((loadedModels) => {
+    client.llm.listLoaded().then(loadedModels => {
       setLoadedModels(loadedModels);
-      console.log('Loaded models:', loadedModels);
+      console.log("Loaded models:", loadedModels);
     });
   }, [isLoading, isUnloading]);
 
@@ -122,36 +144,45 @@ function App() {
   };
 
   const loadModel = async (model: string) => {
-    if (!model || model === '') {
+    if (!model || model === "") {
       return;
     }
-    const opts = {
+    const opts: LLMLoadModelOpts = {
       onProgress: (progress: number) => {
         setLoadingProgress(progress);
         console.log(progress);
       },
       noHup: true,
-    } satisfies LLMLoadModelOpts;
-    
+    };
+
     setIsLoading(true);
     const result = await client.llm.load(model, opts);
     setIsLoading(false);
     const info = await result.getModelInfo();
     console.log(`Model loaded: ${info?.identifier}`);
-    setLoadedModels([...loadedModels, { identifier: info?.identifier || '', address: info?.address || '' }]);
+    setLoadedModels([
+      ...loadedModels,
+      { identifier: info?.identifier || "", address: info?.address || "" },
+    ]);
   };
 
   return (
     <div className="App">
       <h1>👾 LM Studio SDK</h1>
       <h2>React App Example</h2>
-      
-      <div style={{ display: 'flex', flexDirection: 'row', width: '100%', justifyContent: 'center', gap: '10px', alignItems: 'center' }}>
+
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          width: "100%",
+          justifyContent: "center",
+          gap: "10px",
+          alignItems: "center",
+        }}
+      >
         <h3>Load local LLMs:</h3>
-        <select 
-          onChange={(e) => loadModel(e.target.value)}
-          style={{ height: '30px' }}
-        >
+        <select onChange={e => loadModel(e.target.value)} style={{ height: "30px" }}>
           <option value="">Select a model to load</option>
           {models.map((model, index) => (
             <option key={index} value={model.address}>
@@ -160,20 +191,20 @@ function App() {
           ))}
         </select>
       </div>
-      {isLoading && 
-      <>
-        <p>Loading...</p>
-        <progress value={loadingProgress} max="1" />
-      </>
-      }
+      {isLoading && (
+        <>
+          <p>Loading...</p>
+          <progress value={loadingProgress} max="1" />
+        </>
+      )}
 
-      {loadedModels.length > 0 
-      && 
+      {loadedModels.length > 0 && (
         <LoadedModelWidget
           client={client}
           loadedModels={loadedModels}
-          onUnloadModel={handleUnloadModel} />
-      }
+          onUnloadModel={handleUnloadModel}
+        />
+      )}
     </div>
   );
 }
