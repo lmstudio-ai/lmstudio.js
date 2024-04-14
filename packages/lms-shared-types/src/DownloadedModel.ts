9ec36aa5d42a744fe6b1a6560463cd29a24315bc
Ryan Huang <ryan@lmstudio.ai>
2024-04-13 22:21:45 -0400

    Comments

--- packages/lms-shared-types/src/DownloadedModel.ts
+++ packages/lms-shared-types/src/DownloadedModel.ts
@@ -6,12 +6,23 @@ import { z } from "zod";
  * @public
  */
 export type DownloadedModel = {
+  /**
+   * The type of the model.
+   */
   type: "llm" | "embedding";
+  /**
+   * The address of the model. Use to load the model.
+   */
   address: string;
+  /**
+   * The size of the model in bytes.
+   */
+  sizeBytes: number;
 };
 export const downloadedModelSchema = z.discriminatedUnion("type", [
   z.object({
     type: z.enum(["llm", "embedding"]),
     address: z.string(),
+    sizeBytes: z.number(),
   }),
 ]);
