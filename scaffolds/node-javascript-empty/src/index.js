db0017dc3f1ca3b62c7be23d73f4decd6ff88ea9
Ryan Huang <ryan@lmstudio.ai>
2024-04-26 15:01:56 -0400

    Polish

--- scaffolds/node-javascript-empty/src/index.js
+++ scaffolds/node-javascript-empty/src/index.js
@@ -2,6 +2,8 @@ const { LMStudioClient } = require("@lmstudio/sdk");
 
 async function main() {
   const client = new LMStudioClient();
+  console.log("Welcome to my new project! Here are the downloaded models:");
   console.log(await client.system.listDownloadedModels());
+  console.log("For more, visit our documentation website at https://lmstudio.ai/docs/welcome");
 }
 main();
--- scaffolds/node-typescript-empty/src/index.ts
+++ scaffolds/node-typescript-empty/src/index.ts
@@ -2,6 +2,8 @@ import { LMStudioClient } from "@lmstudio/sdk";
 
 async function main() {
   const client = new LMStudioClient();
+  console.log("Welcome to my new project! Here are the downloaded models:");
   console.log(await client.system.listDownloadedModels());
+  console.log("For more, visit our documentation website at https://lmstudio.ai/docs/welcome");
 }
 main();
