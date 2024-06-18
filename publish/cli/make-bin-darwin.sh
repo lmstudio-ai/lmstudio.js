8182837cff1be390e61fc3da109bd95b37ef2413
Daniel Bevenius <dbevenius@lmstudio.ai>
2024-06-18 15:01:01 +0200

    Comment out macos signing for testing CI build

--- publish/cli/make-bin-darwin.sh
+++ publish/cli/make-bin-darwin.sh
@@ -41,32 +41,32 @@ fi
 
 cp "${NODE_DIR}/bin/node" "${DIST_DIR}/${EXE_NAME}"
 
-if ! command -v codesign &> /dev/null
-then
-    echo "Warning: codesign could not be found"
-    exit 1
-fi
+#if ! command -v codesign &> /dev/null
+#then
+#    echo "Warning: codesign could not be found"
+#    exit 1
+#fi
 
-codesign --remove-signature "${DIST_DIR}/${EXE_NAME}"
+#codesign --remove-signature "${DIST_DIR}/${EXE_NAME}"
 
-postject "${DIST_DIR}/${EXE_NAME}" NODE_SEA_BLOB ./temp/sea-prep.blob \
-  --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
-  --macho-segment-name NODE_SEA
+#postject "${DIST_DIR}/${EXE_NAME}" NODE_SEA_BLOB ./temp/sea-prep.blob \
+#  --sentinel-fuse NODE_SEA_FUSE_fce680ab2cc467b6e072b8b5df1996b2 \
+#  --macho-segment-name NODE_SEA
 
-if [[ -z "${APPLE_SIGNING_IDENTITY}" ]]; then
-    echo "Warning: APPLE_SIGNING_IDENTITY is not set"
-    exit 1
-fi
+#if [[ -z "${APPLE_SIGNING_IDENTITY}" ]]; then
+#    echo "Warning: APPLE_SIGNING_IDENTITY is not set"
+#    exit 1
+#fi
 
-if [[ -n "${DIST_DIR}" ]] && [[ -n "${EXE_NAME}" ]]; then
-    codesign --sign "${APPLE_SIGNING_IDENTITY}" --options runtime --entitlements entitlements.plist "${DIST_DIR}/${EXE_NAME}"
-    if [ "$LMS_SKIP_NOTARIZATION" = "1" ] || [ "$LMS_SKIP_NOTARIZATION" = "true" ]; then
-        echo "LMS_SKIP_NOTARIZATION is set. Skipping notarization..."
-    else
-        zip -r "${DIST_DIR}/${EXE_NAME}.zip" "${DIST_DIR}/${EXE_NAME}"
-        xcrun notarytool submit "${DIST_DIR}/${EXE_NAME}.zip" --keychain-profile "AC_PASSWORD" --wait
-    fi
-else
-    echo "Warning: DIST_DIR or EXE_NAME is not set"
-    exit 1
-fi
+#if [[ -n "${DIST_DIR}" ]] && [[ -n "${EXE_NAME}" ]]; then
+#    codesign --sign "${APPLE_SIGNING_IDENTITY}" --options runtime --entitlements entitlements.plist "${DIST_DIR}/${EXE_NAME}"
+#   if [ "$LMS_SKIP_NOTARIZATION" = "1" ] || [ "$LMS_SKIP_NOTARIZATION" = "true" ]; then
+#       echo "LMS_SKIP_NOTARIZATION is set. Skipping notarization..."
+#   else
+#       zip -r "${DIST_DIR}/${EXE_NAME}.zip" "${DIST_DIR}/${EXE_NAME}"
+#       xcrun notarytool submit "${DIST_DIR}/${EXE_NAME}.zip" --keychain-profile "AC_PASSWORD" --wait
+#   fi
+#else
+#   echo "Warning: DIST_DIR or EXE_NAME is not set"
+#   exit 1
+#fi
